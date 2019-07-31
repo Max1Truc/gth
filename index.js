7c316f8fb7dac015a0f59cf076073f39f4d66760
Max1Truc <maxitruc.bonbec@gmail.com>
2019-07-31 19:48:49 +0200

    First working commit

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
 #!/usr/bin/env node
 
-const prompts = require("prompts")
-const fs = require("fs")
-const path = require("path")
+if (process.argv.length != 4) {
+  console.log(
+    "Usage:\n$ gth <Up-to-date translation file> <Translation file to update>"
+  );
+  process.exit(1);
+}
 
-async function getJSON(filename, callback) {
-  //filename = path.join(__dirname, filename)
-  return await fs.readFile(filename, {encoding: 'utf-8'}, function(err,data){
-    if (!err) {
-      data = JSON.parse(data)
-      if (callback) callback(data)
-      else return data
-    } else {
-      console.log(err);
-    }
+var base = process.argv[2],
+  branch = process.argv[3];
+
+process.on("unhandledRejection", function(err) {
+  console.log(err);
+});
+
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function readLineAsync(message) {
+  return new Promise((resolve, reject) => {
+    rl.question(message, answer => {
+      resolve(answer);
+    });
   });
 }
 
+function getJSON(filename) {
+  return JSON.parse(fs.readFileSync(filename, { encoding: "utf-8" }));
+}
+
 async function writeJSON(filename, data) {
-  //filename = path.join(__dirname, filename);
   data = new Uint8Array(Buffer.from(data));
-  fs.writeFile(filename, data, (err) => {
+  fs.writeFile(filename, data, err => {
     if (err) throw err;
-    console.log('The file has been updated!');
+    console.log("The file has been updated!");
+    process.exit();
   });
 }
 
 (async () => {
-  var file1 = await getJSON("english.json")
-  var file2 = await getJSON("french.json")
-console.log(file1)
+  var file1 = getJSON(base);
+  var file2 = getJSON(branch);
+
   for (i in file1) {
     if (!file2[i]) {
-      file2[i] = await prompts({
-        type: 'text',
-        name: 'translation',
-        message: `What is the translation for "${file1[i]}"?`
-      }).translation;
+      file2[i] = await readLineAsync(
+        `What is the translation for "${file1[i]}" (${i})? `
+      );
     }
   }
 
-  writeJSON("french.json", JSON.stringify(file2))
+  writeJSON(branch, JSON.stringify(file2, null, "\t"));
 })();
